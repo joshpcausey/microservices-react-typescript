2ef2e070b5c78fd00d16dcf5043e5aa1fe1be8f6
Joshua Causey <joshpcausey@gmail.com>
2024-11-22 17:57:56 -0600

    add prettier, configure ts

---
+++ posts/index.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { randomBytes } from 'crypto';
+
+const app = express();
+app.use(bodyParser.json());
+
+const posts: Map<string, string> = new Map();
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+// test
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log('listening on port 4000');
+  console.log('test');
+});
+
+interface PostHash {
+  key: string;
+  title: string;
+}

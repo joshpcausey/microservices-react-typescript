42437cc4aecd7d864ed068dbf292f402e4ed201d
Joshua Causey <joshpcausey@gmail.com>
2024-11-23 17:38:38 -0600

    add frontend for posts and fix cors errors

---
+++ client/src/PostCreate.tsx
@@ -0,0 +1,33 @@
+import React, { SyntheticEvent, useState } from 'react';
+import axios from 'axios';
+
+const PostCreate = () => {
+  const [title, setTitle] = useState('');
+
+  const onSubmit = async (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    await axios.post('http://localhost:4000/posts', {
+      title,
+    });
+
+    setTitle('');
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+          <button className="btn btn-primary">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default PostCreate;
